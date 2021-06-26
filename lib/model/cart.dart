efa37a9be63749657e4389dfdea1816e2eb5dd07
Fiction_NTV <thevien898@gmail.com>
2021-06-26 13:25:48 +0700

    add model cart

---
+++ lib/model/cart.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cart {
+  final String? name;
+  final String? image;
+  final double? price;
+  final int? quantity;
+  Cart({this.name, this.image, this.price, this.quantity});
+}
