629946babb11fe96a1add7f044e7a0289172505d
Fiction_NTV <thevien898@gmail.com>
2021-06-26 23:29:14 +0700

    update welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,40 +8,39 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 50),
+          padding: EdgeInsets.symmetric(horizontal: 80),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+            children: <Widget>[
               Container(
                 height: 600,
                 width: double.infinity,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
+                  children: <Widget>[
                     Container(
                       height: 350,
                       width: double.infinity,
                       decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("images/shopping.png"),
-                        ),
-                      ),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("images/shopping.png"))),
                     ),
                     Text(
                       "Welcome",
                       style: TextStyle(
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
                     ),
                     Container(
                       height: 60,
                       width: double.infinity,
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+
+                        children: <Widget>[
                           Text(
                             "Ready To Start Shopping Sign Up",
                             style: TextStyle(fontSize: 18),
@@ -53,52 +52,51 @@ class WelcomeScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-                    Column(
-                      children: [
-                        Container(
-                          height: 45,
-                          width: double.infinity,
-                          child: RaisedButton(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text("Sign Up",
-                                style: TextStyle(
-                                    fontSize: 18, color: Colors.white)),
-                            color: Color(0xff746bc9),
-                            onPressed: () {
-                              Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                  builder: (ctx) => SignUp(),
-                                ),
-                              );
-                            },
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                        color: Color(0xff746bc9),
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (ctx) => SignUp(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Already have an account?",
+                          style: TextStyle(
+                            fontSize: 18,
                           ),
                         ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Already have an account?",
-                              style: TextStyle(
-                                fontSize: 18,
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                builder: (ctx) => SignIn(),
                               ),
+                            );
+                          },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff7746bc9),
                             ),
-                            GestureDetector(
-                              child: Text(
-                                "Login",
-                                style: TextStyle(
-                                    fontSize: 18, color: Color(0xff746bc9)),
-                              ),
-                              onTap: () {
-                                Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                    builder: (ctx) => SignIn(),
-                                  ),
-                                );
-                              },
-                            )
-                          ],
-                        ),
+                          ),
+                        )
                       ],
                     ),
                   ],
@@ -111,7 +109,3 @@ class WelcomeScreen extends StatelessWidget {
     );
   }
 }
-
-
-
-// https://www.youtube.com/playlist?list=PLQrn8asEsczpy2AxA3sKSD99wMMWXeAlL
