0e048a1b4cf8cce97e2847a69a351373f6ae82ae
Fiction_NTV <thevien898@gmail.com>
2021-06-26 23:28:44 +0700

    fix size display single product

--- lib/widgets/single_product.dart
+++ lib/widgets/single_product.dart
@@ -1,43 +1,55 @@
 import 'package:flutter/material.dart';
+
 class SingleProduct extends StatelessWidget {
   final String image;
   final double price;
   final String name;
   SingleProduct({required this.image, required this.name, required this.price});
-
   @override
   Widget build(BuildContext context) {
+    double width, height;
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
     return Card(
       child: Container(
-        height: 250,
-        width: 170,
+        height: height * 0.4,
+        width: width * 0.2 * 2 + 10,
         child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
+          children: <Widget>[
+            Expanded(
+              flex: 3,
               child: Container(
-                height: 170,
-                width: 150,
-                decoration: BoxDecoration(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Container(
+                  width: 160,
+                  decoration: BoxDecoration(
                     image: DecorationImage(
                       fit: BoxFit.fill,
                       image: NetworkImage(image),
-                    )),
+                    ),
+                  ),
+                ),
               ),
             ),
-            Text(
-              "\$ ${price.toString()}",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17,
-                  color: Color(0xff9b96d6)),
-            ),
-            Container(
-              child: Text(
-                "$name",
-                style: TextStyle(fontSize: 17),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(
+                    "\$ ${price.toString()}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Color(0xff9b96d6)),
+                  ),
+                  Container(
+                    child: Text(
+                      name,
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  )
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
