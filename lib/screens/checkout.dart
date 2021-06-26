7ebacb64ac7b852eaef4f79e9c9173a2ee036091
Fiction_NTV <thevien898@gmail.com>
2021-06-26 23:23:24 +0700

    update checkout

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -1,8 +1,11 @@
-import 'package:e_commerce_app/model/product.dart';
+
+import 'package:e_commerce_app/model/cart.dart';
 import 'package:e_commerce_app/provider/product_provider.dart';
-import 'package:e_commerce_app/screens/cart_screen.dart';
+import 'package:e_commerce_app/screens/home_screen.dart';
 import 'package:e_commerce_app/widgets/cart_single_product.dart';
+import 'package:e_commerce_app/widgets/my_button.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:provider/provider.dart';
 
 class CheckOut extends StatefulWidget {
@@ -11,108 +14,166 @@ class CheckOut extends StatefulWidget {
 }
 
 class _CheckOutState extends State<CheckOut> {
-  final TextStyle myStyle = TextStyle(fontSize: 18);
+  TextStyle myStyle = TextStyle(
+    fontSize: 18,
+  );
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   late ProductProvider productProvider;
 
-  Widget _buildBottomDetail({String? startName, String? endName}) {
+  Widget _buildBottomSingleDetail({String? startName, String? endName}) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
+      children: <Widget>[
         Text(
-          "$startName",
+          startName!,
           style: myStyle,
         ),
         Text(
-          "$endName",
+          endName!,
           style: myStyle,
         ),
       ],
     );
   }
 
+  double? total;
+  List<Cart>? myList;
+
+  Widget _buildButton() {
+    return Column(
+      children: [
+        Container(
+          height: 50,
+          child: MyButton(
+            name: "Buy",
+            onPressed: () {
+            },
+          ),
+        )
+      ],
+    );
+
+  }
+
+  @override
+  void initState() {
+    productProvider = Provider.of<ProductProvider>(context, listen: false);
+    myList = productProvider.checkOutModelList;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    productProvider = Provider.of<ProductProvider>(context);
+    double subTotal = 0;
+    double discount = 3;
+    double discountRupees;
+    double shipping = 60;
 
-    return Scaffold(
-      bottomNavigationBar: Container(
-        height: 60,
-        width: 100,
-        margin: EdgeInsets.symmetric(horizontal: 10),
-        padding: EdgeInsets.only(bottom: 10),
-        child: RaisedButton(
-          color: Color(0xff746bc9),
-          child: Text(
-            "Buy",
-            style: TextStyle(fontSize: 18, color: Colors.white),
+    productProvider = Provider.of<ProductProvider>(context);
+    productProvider.getCheckOutModelList.forEach((element) {
+      subTotal += (element.price! * (element.quantity as int));
+    });
+
+    discountRupees = discount / 100 * subTotal;
+    total = subTotal + shipping - discountRupees;
+    if (productProvider.checkOutModelList.isEmpty) {
+      total = 0.0;
+      discount = 0.0;
+      shipping = 0.0;
+    }
+
+    return WillPopScope(
+      onWillPop: () async {
+        return Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (ctx) => HomeScreen(),
           ),
-          onPressed: () {},
-        ),
-      ),
-      appBar: AppBar(
-        title: Text(
-          "CheckOut Page",
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.of(context).pushReplacement(
-              MaterialPageRoute(
-                builder: (ctx) => CartScreen(),
-              ),
-            );
-          },
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
+        ) as Future<bool>;
+      },
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("CheckOut Page", style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          leading: IconButton(
             icon: Icon(
-              Icons.notifications_none,
+              Icons.arrow_back,
               color: Colors.black,
             ),
-          )
-        ],
-      ),
-      body: Container(
+            onPressed: () {
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (ctx) => HomeScreen(),
+                ),
+              );
+            },
+          ),
+          actions: <Widget>[
+          ],
+        ),
+        bottomNavigationBar: Container(
+          height: 70,
+          width: 100,
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          padding: EdgeInsets.only(bottom: 15),
+          child: _buildButton(),
+        ),
+        body: Container(
           padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListView.builder(
-                itemCount: productProvider.getCartListLength,
-                itemBuilder: (context, index) {
-                  return CartSingleProduct(
-                    image: productProvider.getCartList[index].image,
-                    name: productProvider.getCartList[index].name,
-                    price: productProvider.getCartList[index].price,
-                    quantity: productProvider.getCartList[index].quantity,
-                  );
-                },
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: ListView.builder(
+                    itemCount: myList!.length,
+                    itemBuilder: (ctx, myIndex) {
+                      return CheckOutSingleProduct(
+                        index: myIndex,
+                        color: myList![myIndex].color,
+                        size: myList![myIndex].size,
+                        image: myList![myIndex].image,
+                        name: myList![myIndex].name,
+                        price: myList![myIndex].price,
+                        quantity: myList![myIndex].quantity,
+                      );
+                    },
+                  ),
+                ),
               ),
-              Container(
-                height: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildBottomDetail(
-                        startName: "Subtotal", endName: "\$ 60.00"),
-                    _buildBottomDetail(startName: "Discount", endName: "3%"),
-                    _buildBottomDetail(
-                        startName: "Shipping", endName: "\$ 60.00"),
-                    _buildBottomDetail(
-                        startName: "Total", endName: "\$ 120.00"),
-                  ],
+              Expanded(
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      _buildBottomSingleDetail(
+                        startName: "Subtotal",
+                        endName: "\$ ${subTotal.toStringAsFixed(2)}",
+                      ),
+                      _buildBottomSingleDetail(
+                        startName: "Discount",
+                        endName: "${discount.toStringAsFixed(2)}%",
+                      ),
+                      _buildBottomSingleDetail(
+                        startName: "Shipping",
+                        endName: "\$ ${shipping.toStringAsFixed(2)}",
+                      ),
+                      _buildBottomSingleDetail(
+                        startName: "Total",
+                        endName: "\$ ${total!.toStringAsFixed(2)}",
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
