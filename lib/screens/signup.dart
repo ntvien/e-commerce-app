e66345503ff0a2202155b3e785daa498ed6821b9
Fiction_NTV <thevien898@gmail.com>
2021-06-26 23:28:17 +0700

    update authentication sign up

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,14 +1,13 @@
+import 'package:e_commerce_app/screens/home_screen.dart';
+import 'package:e_commerce_app/screens/signin.dart';
 import 'package:e_commerce_app/widgets/change_screen.dart';
 import 'package:e_commerce_app/widgets/my_button.dart';
 import 'package:e_commerce_app/widgets/my_text_form_field.dart';
 import 'package:e_commerce_app/widgets/password_text_form_field.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'signin.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -17,104 +16,197 @@ class SignUp extends StatefulWidget {
 
 final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-// Regular Expressions
 String p =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
 RegExp regExp = new RegExp(p);
 bool obserText = true;
+final TextEditingController email = TextEditingController();
+final TextEditingController userName = TextEditingController();
+final TextEditingController phoneNumber = TextEditingController();
+final TextEditingController password = TextEditingController();
+final TextEditingController address = TextEditingController();
 
-String email = "";
-String password = "";
+bool isMale = true;
+bool isLoading = false;
 
 class _SignUpState extends State<SignUp> {
-  void validation() async {
-    final FormState? _form = _formKey.currentState;
-    if (!_form!.validate()) {
-      try {
-        AuthResult result = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(email: email, password: password);
-        print(result.user.uid);
-      } on PlatformException catch (e) {
-        print(e.message.toString());
-        _scaffoldKey.currentState!.showSnackBar(
-          SnackBar(
-            content: Text(e.message.toString()),
-          ),
-        );
+  void submit() async {
+    UserCredential result;
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email.text, password: password.text);
+      print(result);
+    } on PlatformException catch (error) {
+      var message = "Please Check Your Internet Connection ";
+      if (error.message != null) {
+        message = error.message!;
       }
+      _scaffoldKey.currentState!.showSnackBar(SnackBar(
+        content: Text(message.toString()),
+        duration: Duration(milliseconds: 600),
+        backgroundColor: Theme.of(context).primaryColor,
+      ));
+      setState(() {
+        isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
+      _scaffoldKey.currentState!.showSnackBar(SnackBar(
+        content: Text(error.toString()),
+        duration: Duration(milliseconds: 600),
+        backgroundColor: Theme.of(context).primaryColor,
+      ));
+
+      print(error);
+    }
+    // FirebaseFirestore.instance.collection("User").doc(result.user.uid).set({
+    //   "UserName": userName.text,
+    //   "UserId": result.user.uid,
+    //   "UserEmail": email.text,
+    //   "UserAddress": address.text,
+    //   "UserGender": isMale == true ? "Male" : "Female",
+    //   "UserNumber": phoneNumber.text,
+    // });
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (ctx) => HomeScreen()));
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  void validation() async {
+    if (userName.text.isEmpty &&
+        email.text.isEmpty &&
+        password.text.isEmpty &&
+        phoneNumber.text.isEmpty &&
+        address.text.isEmpty) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("All Flied Are Empty"),
+        ),
+      );
+    } else if (userName.text.length < 6) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Name Must Be 6 "),
+        ),
+      );
+    } else if (email.text.isEmpty) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Email Is Empty"),
+        ),
+      );
+    } else if (!regExp.hasMatch(email.text)) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Please Try Valid Email"),
+        ),
+      );
+    } else if (password.text.isEmpty) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Password Is Empty"),
+        ),
+      );
+    } else if (password.text.length < 8) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Password  Is Too Short"),
+        ),
+      );
+    } else if (phoneNumber.text.length < 11 || phoneNumber.text.length > 11) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Phone Number Must Be 11 "),
+        ),
+      );
+    } else if (address.text.isEmpty) {
+      _scaffoldKey.currentState!.showSnackBar(
+        SnackBar(
+          content: Text("Address Is Empty "),
+        ),
+      );
     } else {
-      print("dddd");
+      submit();
     }
   }
 
   Widget _buildAllTextFormField() {
     return Container(
-      height: 330,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
+        children: <Widget>[
           MyTextFormField(
-            validator: (value) {
-              if (value == "") {
-                return "Please Fill UserName";
-              } else if (value!.length < 6) {
-                return "UserName Is Too Short";
-              }
-              return "";
-            },
-            name: "User Name",
+            name: "UserName",
+            controller: userName,
+          ),
+          SizedBox(
+            height: 10,
           ),
           MyTextFormField(
-            validator: (value) {
-              if (value == "") {
-                return "Please Fill Email";
-              } else if (!regExp.hasMatch(value!)) {
-                return "Email Is Not Invalid";
-              }
-              return "";
-            },
-            onChanged: (value) {
-              setState(() {
-                email = value;
-              });
-            },
             name: "Email",
+            controller: email,
+          ),
+          SizedBox(
+            height: 10,
           ),
           PasswordTextFormField(
             obserText: obserText,
-            validator: (value) {
-              if (value == "") {
-                return "Please Fill Password";
-              } else if (value!.length < 8) {
-                return "Password Is Too Short";
-              }
-              return "";
-            },
-            onChanged: (value) {
-              setState(() {
-                password = value;
-                print(password);
-              });
-            },
+            controller: password,
             name: "Password",
             onTap: () {
               FocusScope.of(context).unfocus();
               setState(() {
-                obserText != obserText;
+                obserText = !obserText;
               });
             },
           ),
-          MyTextFormField(
-            validator: (value) {
-              if (value == "") {
-                return "Please Fill Phone Number";
-              } else if (value!.length != 10) {
-                return "Phone Number Must Be 10";
-              }
-              return "";
+          SizedBox(
+            height: 10,
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isMale = !isMale;
+              });
             },
+            child: Container(
+              height: 60,
+              padding: EdgeInsets.only(left: 10),
+              width: double.infinity,
+              decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+              child: Center(
+                child: Row(
+                  children: [
+                    Text(
+                      isMale == true ? "Male" : "Female",
+                      style: TextStyle(color: Colors.black87, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          MyTextFormField(
             name: "Phone Number",
+            controller: phoneNumber,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          MyTextFormField(
+            name: "Address",
+            controller: address,
           ),
         ],
       ),
@@ -123,21 +215,27 @@ class _SignUpState extends State<SignUp> {
 
   Widget _buildBottomPart() {
     return Container(
-      height: 400,
       margin: EdgeInsets.symmetric(horizontal: 10),
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
+        children: <Widget>[
           _buildAllTextFormField(),
-          MyButton(
-            name: "SignUp",
-            onPressed: () {
-              validation();
-            },
+          SizedBox(
+            height: 10,
           ),
+          isLoading == false
+              ? MyButton(
+                  name: "SignUp",
+                  onPressed: () {
+                    validation();
+                  },
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
           ChangeScreen(
-            name: "Sign In",
+            name: "Login",
             whichAccount: "I Have Already An Account!",
             onTap: () {
               Navigator.of(context).pushReplacement(
@@ -146,7 +244,7 @@ class _SignUpState extends State<SignUp> {
                 ),
               );
             },
-          )
+          ),
         ],
       ),
     );
@@ -156,37 +254,28 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Container(
+      body: ListView(
+        children: [
+          Container(
+            height: 200,
             child: Column(
-              children: [
-                Container(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                _buildBottomPart(),
               ],
             ),
           ),
-        ),
+          Container(
+            height: 500,
+            child: _buildBottomPart(),
+          ),
+        ],
       ),
     );
   }
